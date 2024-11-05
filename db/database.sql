d6924b8e7348592983c5d7cfd4367cf950efbd49
Victor Lorencio <victorlorencio@gmail.com>
2024-11-05 18:23:51 +0100

    Archivo referencia de la base de datos

---
+++ db/database.sql
@@ -0,0 +1,13 @@
+/*Referencia de la base de datos*/
+
+CREATE DATABASE IF NOT EXIST cursos_db;
+
+USE cursos_db;
+
+CREATE TABLE catalogo_cursos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    categoria VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255) NOT NULL,
+    duracion INT NOT NULL,
+)
