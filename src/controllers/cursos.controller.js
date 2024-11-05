fbee0b8dc6d0aa69bd99788cbde0b301cfad5754
Victor Lorencio <victorlorencio@gmail.com>
2024-11-05 21:46:14 +0100

    Añadir ruta para cursos y carpeta rutas y carpeta para controladores

---
+++ src/controllers/cursos.controller.js
@@ -0,0 +1,7 @@
+export const getCursos = (req, res) => res.send('Estos son los cursos disponibles') 
+
+export const createCursos = (req, res) =>  res.send('Crear cursos') 
+
+export const updateCursos = (req, res) =>  res.send('Actualizar cursos') 
+
+export const deleteCursos = (req, res) =>  res.send('Eliminar cursos') 
--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -1,16 +1,19 @@
 import { Router } from "express"; //Sirve para crear rutas en la API
-
+import { getCursos } from "../controllers/cursos.controller.js"; // Importamos las funccion de listar cursos
+import { createCursos } from "../controllers/cursos.controller.js"; // Importamos la funcion de crear cursos
+import { updateCursos } from "../controllers/cursos.controller.js"; // Importamos la funcion de actualizar cursos
+import { deleteCursos } from "../controllers/cursos.controller.js"; // Importamos la funcion de eliminar cursos
 const router = Router(); // Creamos un objeto router (enroutador) para manejar las rutas
 
 /*Endpoints o rutas de la API*/
 // Ruta para mostrar los cursos disponibles
-router.get('/cursos', (req, res) => { res.send('Estos son los cursos disponibles') }); 
+router.get('/cursos', getCursos); 
  // Ruta para crear cursos
-router.post('/cursos', (req, res) => { res.send('Crear cursos') });
+router.post('/cursos',createCursos);
 // Ruta para actualizar cursos
-router.put('/cursos', (req, res) => { res.send('Actualizar cursos') }); 
+router.put('/cursos', updateCursos);
 // Ruta para eliminar cursos
-router.delete('/cursos', (req, res) => { res.send('Eliminando cursos') }); 
+router.delete('/cursos',deleteCursos); 
 
 
 export default router; // Exportamos el objeto router
