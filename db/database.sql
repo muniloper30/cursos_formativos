1e326d660d0ba5c96510b69c9450f020589bbbc1
Victor Lorencio <victorlorencio@gmail.com>
2024-11-05 20:53:27 +0100

    Modificación de archivos db y reorganización de archivos en carpetas

--- db/database.sql
+++ db/database.sql
@@ -1,6 +1,6 @@
 /*Referencia de la base de datos*/
 
-CREATE DATABASE IF NOT EXIST cursos_db;
+CREATE DATABASE IF NOT EXISTS cursos_db;
 
 USE cursos_db;
 
@@ -9,5 +9,5 @@ CREATE TABLE catalogo_cursos (
     nombre VARCHAR(255) NOT NULL,
     categoria VARCHAR(255) NOT NULL,
     descripcion VARCHAR(255) NOT NULL,
-    duracion INT NOT NULL,
+    duracion INT NOT NULL
 )
