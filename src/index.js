d36ea99c2044d58b6c307e7cc5745384e0ca05b3
Victor Lorencio <victorlorencio@gmail.com>
2024-11-23 13:00:32 +0100

    correccion ortografia

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express from 'express'; // Impot expres para crear el servidor
+import express from 'express'; // Import expres para crear el servidor
 import cursosRoutes from './routes/cursos.routes.js'; // Importamos las rutas de cursos, podemos darle el nombre que queramos ya que es un archivo propio
 import indexRoutes from './routes/index.routes.js'; // Importamos las rutas de index, podemos darle el nombre que queramos ya que es un archivo propio
 import path from 'path'; // Importamos path para trabajar con las rutas de los archivos
