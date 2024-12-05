11d3e329d9c3e0774fb1a77963bbfe48db7e27b3
Victor Lorencio <victorlorencio@gmail.com>
2024-12-05 15:22:03 +0100

    Modificar index para deployed

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import indexRoutes from './routes/index.routes.js'; // Importamos las rutas de i
 import path from 'path'; // Importamos path para trabajar con las rutas de los archivos
 
 const app = express();// Creamos el servidor
+const port =  process.env.PORT || 3000; // Creamos una variable para el puerto del servidor, si no existe la variable de entorno PORT, usamos el puerto 3000
+
 
 //Creamos un middleware para que servir los archivos estaticos de "public"
 app.use(express.static(path.resolve('public'))); // Usamos el metodo use de express para usar el middleware de archivos estaticos, le pasamos la ruta de la carpeta public con path.resolve
+
+
 
 app.use(indexRoutes); // Usamos las rutas de index
 app.use("/api",cursosRoutes); // Usamos las rutas de cursos
 
 
-app.listen(3000)
-console.log("Server running on port 3000");
+app.listen(port, () => {
+    console.log(`Port runing in http://localhost:${port}`);
+}); // Iniciamos el servidor en el puerto que definimos en la variable port
