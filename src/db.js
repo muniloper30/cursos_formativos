03cd7b83c77453efabe70921794e4c77480cbf65
Victor Lorencio <victorlorencio@gmail.com>
2024-12-05 16:33:05 +0100

    Archivo .env para variables de entorno de la base de datos, modificación en db.js para  cargar variables en vercel, instalar dotenv para poder usarlo

--- src/db.js
+++ src/db.js
@@ -1,10 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config(); // Carga las variables de entorno desde el archivo .env
+
 import { createPool } from "mysql2/promise";
 
-export const pool = createPool({ // Creamos la conexion a la base de datos
-    host: "localhost",
-    user: "root",
-    password: "9325921992",
-    port: 3306,
-    database: "cursos_db"
+export const pool = createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT || 3306,
+    database: process.env.DATABASE_NAME
 });
-
